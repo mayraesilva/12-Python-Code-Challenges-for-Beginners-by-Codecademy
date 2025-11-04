49e8105266f8d39faba563feda4c2c968c1a60da
Mayra Silva <mayraeduarda2002@gmail.com>
2025-11-04 18:10:08 -0300

    Task 1

---
+++ CH-1-Radians-into-degrees.py
@@ -0,0 +1,10 @@
+"""
+1. Convert radians into degrees
+Write a function in Python that accepts one numeric parameter. This parameter will be the measure of an angle in radians. 
+The function should convert the radians into degrees and then return that value.
+
+While you might find a Python library to do this for you, you should write the function yourself. 
+One hint you get is that you’ll need to use Pi in order to solve this problem. You can import the value for Pi from Python’s math module.
+
+"""
+
